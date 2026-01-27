dd85e0d165c63cf828e6084974b3adee94d598b8
krameraad <eliaspowder@gmail.com>
2026-01-27 23:55:32 +0100

    docstring, various improvements in the test case (to imported into the main program)

--- interface.py
+++ interface.py
@@ -3,10 +3,22 @@ import sys
 
 
 def interface() -> int:
-    print("0: Exit program\n"
-          "1: Regenerate the maze\n"
-          "2: Toggle path to the exit\n"
-          "3: Change maze colors\n")
+    """
+    Display options for the user and forward the results to the program.
+
+    - `0`: Exits the program (handled in function).
+    - `1`: Regenerate the maze.
+    - `2`: Toggle path to the exit.
+    - `3`: Change the maze colors.
+    - `default`: Returns `0` (invalid input).
+
+    Returns:
+        int: Option selected by the user, which is either 0, 1, 2 or 3.
+    """
+    print("\033[1;93m0\033[0m: Exit program\n"
+          "\033[1;93m1\033[0m: Regenerate maze\n"
+          "\033[1;93m2\033[0m: Toggle path to the exit\n"
+          "\033[1;93m3\033[0m: Change maze colors\n")
     match input("Select an option: "):
         case "0":
             sys.exit()
@@ -16,14 +28,22 @@ def interface() -> int:
             return 2
         case "3":
             return 3
-        case _:
-            input("\033[91mInvalid option, press enter to try again\033[0;m")
-            os.system("cls" if os.name == "nt" else "clear")
     return 0
 
 
 if __name__ == "__main__":
+    os.system("cls" if os.name == "nt" else "clear")
+    msg = "\033[1;93mWelcome to A-Maze-ing!\033[0m"
     while True:
-        option = interface()
-        if option:
-            print(option)
+        print(msg)
+        print("\n\033[2m<Maze>\033[0m\n")
+        match interface():
+            case 1:
+                msg = "\033[2mRegenerated maze.\033[0m"
+            case 2:
+                msg = "\033[2mToggled path.\033[0m"
+            case 3:
+                msg = "\033[2mChanged maze colors.\033[0m"
+            case _:
+                msg = "\033[91mInvalid option, try again.\033[0;m"
+        os.system("cls" if os.name == "nt" else "clear")
