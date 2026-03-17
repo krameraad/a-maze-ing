6bdd23695c499e22956378eb3d9dc4af2666463f
ekramer <ekramer@student.42.fr>
2026-03-17 21:31:42 +0100

    new carve_passage(), entry/exit within logo validation

--- mazegen/maze.py
+++ mazegen/maze.py
@@ -41,6 +41,9 @@ class Maze:
             raise ValueError("Entry and exit share coordinates.")
 
         self.logo = self._logo_cells()
+        if self.entry in self.logo or self.exit in self.logo:
+            raise ValueError("Entry or exit overlap with the 42 pattern.")
+
         # Create grid of cells.
         self.grid: list[list[Cell]] = []
         for y in range(self.height):
@@ -115,37 +118,27 @@ class Maze:
         neighbor: tuple[int, int],
     ) -> None:
         """Remove walls between two adjacent cells."""
+        DIRECTIONS = {
+            (0, +1): 0b1110,
+            (-1, 0): 0b1101,
+            (0, -1): 0b1011,
+            (+1, 0): 0b0111}
 
-        x1, y1 = current
-        x2, y2 = neighbor
+        x, y = current[0] - neighbor[0], current[1] - neighbor[1]
 
-        cell1 = self.grid[y1][x1]
-        cell2 = self.grid[y2][x2]
-
-        if x2 == x1 and y2 == y1 - 1:  # North
-            cell1.open_wall("N")
-            cell2.open_wall("S")
-
-        elif x2 == x1 + 1 and y2 == y1:  # East
-            cell1.open_wall("E")
-            cell2.open_wall("W")
-
-        elif x2 == x1 and y2 == y1 + 1:  # South
-            cell1.open_wall("S")
-            cell2.open_wall("N")
-
-        elif x2 == x1 - 1 and y2 == y1:  # West
-            cell1.open_wall("W")
-            cell2.open_wall("E")
+        cell = self.grid[current[1]][current[0]]
+        cell.walls &= DIRECTIONS[(x, y)]
+        cell = self.grid[neighbor[1]][neighbor[0]]
+        cell.walls &= DIRECTIONS[(-x, -y)]
 
     def _open_dead_ends(self) -> None:
         """Make all eligible dead-ends into straight corridors."""
         # Which dead-end orientation corresponds to which direction.
         DIRECTIONS = {
-            0b1110: (0, +1),
-            0b1101: (-1, 0),
-            0b1011: (0, -1),
-            0b0111: (+1, 0)}
+            0b1110: (0, +1),  # Opening is north, target is south
+            0b1101: (-1, 0),  # Opening is east, target is west
+            0b1011: (0, -1),  # Opening is south, target is north
+            0b0111: (+1, 0)}  # Opening is west, target is east
 
         for y, row in enumerate(self.grid):
             for x, cell in enumerate(row):
@@ -163,9 +156,8 @@ class Maze:
     def _logo_cells(self) -> set[tuple[int, int]]:
         """Return cells that form the '42' logo in the center of the maze."""
         cells: set[tuple[int, int]] = set()
-        cx = self.width // 2 - 3
-        cy = self.height // 2 - 2
 
+        # Maze should be big enough to contain the logo.
         MIN_WIDTH, MIN_HEIGHT = 9, 7
         if self.width < MIN_WIDTH or self.height < MIN_HEIGHT:
             print(f"Maze too small for '42' logo "
@@ -188,9 +180,9 @@ class Maze:
             (4, 4), (5, 4), (6, 4),          # bottom bar
         ]
 
-        for dc, dr in fourtytwo:
-            col = cx + dc
-            row = cy + dr
-            if 0 <= col < self.width and 0 <= row < self.height:
-                cells.add((col, row))
+        offset_x = self.width // 2 - 3
+        offset_y = self.height // 2 - 2
+        for x, y in fourtytwo:
+            cells.add((x + offset_x, y + offset_y))
+
         return cells
